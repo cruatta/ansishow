aed2cc745c92ab8310212fc77801f852c5d12fdf
Cameron Ruatta <cameron@ruatta.com>
2022-07-09 16:34:24 -0700

    looping works

--- ansishow/__init__.py
+++ ansishow/__init__.py
@@ -1,23 +1,27 @@
 import pygame
 import sys
 
+
+def calc_x_y(_screen, _graphic_x):
+    return _screen.get_size()[0] / 2 - _graphic_x / 2, _screen.get_size()[1]
+
+
+def load_image(path: str):
+    img = pygame.image.load(path)
+    img_x, img_y = img.get_size()
+    return img, img_x, img_y
+
+
 pygame.init()
 graphic = pygame.image.load(sys.argv[1])
 graphic_x, graphic_y = graphic.get_size()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen.fill((0, 0, 0))
 clock = pygame.time.Clock()
 running = 1
 x, y = screen.get_size()[0] / 2 - graphic_x / 2, screen.get_size()[1]
 
-
-# WIP
-class ANSI(pygame.sprite.Sprite):
-    def __init__(self, image_file, speed, location):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+background = pygame.surface.Surface((screen.get_width(), screen.get_height()))
+background.fill((0, 0, 0))
 
 
 while running:
@@ -29,7 +33,14 @@ while running:
             y -= 100
             screen.blit(graphic, (x, y))
             pygame.display.flip()
+    screen.blit(background, (0, 0))
     screen.blit(graphic, (x, y))
     pygame.display.flip()  # Update screen
     y -= 1
+
+    if y < -graphic_y - 100:
+        graphic, graphic_x, graphic_y = load_image('test.png')
+        x, y = calc_x_y(screen, graphic_x)
+
+
 
